1c42a6f6bfd6f2073a1a84256fac3d6dca80c69b
Nirvana <lamabruno55@gmail.com>
2023-09-12 11:37:38 +0545

    feat: simple crud

--- server/routes/home_route.py
+++ server/routes/home_route.py
@@ -2,6 +2,11 @@ from fastapi import  APIRouter,Depends
 from middleware.auth_middleware import protected_route
 from server.models.vehicle_modle import AddVehicle,GetVehicle,UpdateVehicle
 from server.database import get_collection
+from bson.objectid import ObjectId
+from bson import json_util
+import json
+from pymongo.collection import ReturnDocument
+
 
 router = APIRouter(dependencies=[Depends(protected_route)])
 collection = get_collection("practice", "vehicles")
@@ -13,14 +18,37 @@ def welcome():
 @router.post("/add-vehicle")
 async def add_vehicle(item:AddVehicle):
     try:
-        await collection.insert_one(item)
+        collection.insert_one(dict(item))
         return {"message":"Vehicle added successfully"}
     except Exception as e:
-        print(f"Error:{e}")
+        return f"Error:{e}"
 
+@router.get("/{id}")
 async def get_vehicle(id:str):
     try:
-        vehicle = await collection.find_one({id})
-        return {"message":"Vehicle fetched successfully", "data": vehicle}
+        vehicle = collection.find_one({"_id":ObjectId(id)})
+        if vehicle is None:
+             return {"message":"Vehicle not found"}
+        return {"message":"Vehicle fetched successfully", "data": json.loads(json_util.dumps(vehicle))}
     except Exception as e:
-        print(f"Error:{e}")
+        return f"Error:{e}"
+
+@router.delete("/{id}")
+async def get_vehicle(id:str):
+    try:
+        vehicle = collection.delete_one({"_id":ObjectId(id)})
+        if vehicle is None:
+                return {"message":"Vehicle not found"}
+        return {"message":"Vehicle removed successfully"}
+    except Exception as e:
+        return f"Error:{e}"
+    
+@router.put("/{id}")
+async def get_vehicle(id:str,item:AddVehicle):
+    try:
+        vehicle = collection.find_one_and_update({"_id":ObjectId(id)},{"$set": dict(item)} ,return_document=ReturnDocument.AFTER)
+        if vehicle is None:
+                return {"message":"Vehicle not found"}
+        return {"message":"Vehicle updated successfully"}
+    except Exception as e:
+        return f"Error:{e}"
